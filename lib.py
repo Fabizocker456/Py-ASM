84bcb4f2a7dc98059d3f411ef7c6153999a79bf4
Fabizocker456 <fabian.schiener@gmx.de>
2022-08-27 20:36:31 +0200

    ...
    It was Penguin lust... at its ugliest.

--- lib.py
+++ lib.py
@@ -15,108 +15,21 @@ def parse(inp:str, code:types.CodeType = None) -> types.CodeType:
     mode = "void"
     modes = {"data":[], "code":[], "meta":[]}
     for line, i in enumerate(inp):
-        if g := re.fullmatch("\\.([a-z_]+)\\s*(?:@.*)?", i.strip()):
-            mode = g.group(1)
-        else:
-            if mode in modes and i.strip():
-                if re.fullmatch("\\s*(?:@.*)?", i.strip()):
-                    continue
-                if "@" in i:
-                    i = i.split("@")[0]
-                modes[mode].append((i.strip(), line))
+        i = i.strip()
+        if i.startswith("@"):
+            continue
+        if "@" in i:
+            i = i.split("@")[0]
+        i = i.strip()
+        i = i.split(" ")
+        i = list(filter(bool, map(lambda o: o.strip(), i)))
+        if not i:
+            continue
+        if i[0].startswith("."):
+            mode = i[0][1:]
+            continue
+        if mode in modes:
+            modes[mode].append((i, line))
+    print(modes)
     # stage III
-    consts = [None]
-    const_names = {}
-    names = []
-    name_names = {}
-    for c, line in modes["data"]:
-        if i := re.fullmatch("const\\s+(.+)", c):
-            i = i.group(1)
-            if g := re.fullmatch("marshal\\s+([a-z_]+)\\s+([0-9a-f]+)", i):
-                obj = marshal.loads(bytes.fromhex(g.group(2)))
-                name = g.group(1)
-            else:
-                raise SyntaxError(f"Could not extract data directive: \"{i}\" (line {line})")
-            consts.append(obj)
-            const_names[name] = len(consts) - 1
-        elif i := re.fullmatch("name\\s+([a-z_]+)\\s+(\\S+)", c):
-            obj = i.group(2)
-            name = i.group(1)
-            if obj not in names:
-                names.append(obj)
-            name_names[name] = names.index(obj)
-    instrs = []
-    for i, line in modes["code"]:
-        if g := re.fullmatch("([a-z_]+)\\s+(\\S+)", i):
-            op = g.group(1)
-            arg = g.group(2)
-            op = op.upper()
-        else:
-            raise SyntaxError(f"Could not extract instruction: \"{i}\" (line {line})")
-        if op not in opmap:
-            raise NameError(f"No such operation: \"{op}\" (line {line})")
-        op = opmap[op]
-        if op in hasjrel or op in hasjabs:
-            try:
-                arg = int(arg)
-            except:
-                raise ValueError(f"Jump target {arg} invalid: must be int (line {line})")
-        elif op in hasconst:
-            if arg not in const_names:
-                raise NameError(f"No registered constant: \"{arg}\" (line {line})")
-            arg = const_names[arg]
-        elif op in hasname:
-            if arg not in name_names:
-                raise NameError(f"No name registered for {arg}")
-            arg = name_names[arg]
-        elif op in hascompare:
-            if arg not in cmp_op:
-                    raise ValueError(f"No comparative operator: \"{arg}\" (line {line})")
-            else:
-                arg = cmp_op.index(arg)
-        else:
-            try:
-                arg = int(arg)
-            except:
-                raise ValueError(f"Argument {arg} not int (line {line})")
-            if arg > 255 or arg < 0:
-                raise ValufError(f"Argument {arg} too large (>255) or too small (<0) (line {line})")
-        instrs.append((op, arg, line))
-    meta = {}
-    for i, line in modes["meta"]:
-        if g := re.fullmatch("([a-z]+)\\s+([a-zA-Z0-9]+)", i):
-            meta[g.group(1)] = g.group(2)
-        else:
-            raise SyntaxError("Bad meta directive: \"{i}\" (line {line})")
-
-    # stage IV
-    prg = b""
-    for op, arg, line in instrs:
-        prg += bytes((op, arg))
-    code = code.replace(co_code = prg)
-    code = code.replace(co_consts = tuple(consts))
-    code = code.replace(co_names = tuple(names))
-    code = code.replace(co_linetable = bytes((2, 1) * (len(prg) // 2)))
-    typemap = {
-        "name": str,
-        "argcount": int,
-        "posonlyargcount": int,
-        "kwonlyargcount": int,
-        "nlocals": int,
-        "varnames": (str,),
-        "cellvars": (str,),
-        "freevars": (str,),
-        "filename": str,
-        "firstlineno": int,
-        "stacksize": int,
-        "flags": int
-    }
-    for i in meta:
-        if i in typemap:
-            j = meta[i]
-            t = typemap[i]
-            if t in (str, int):
-                code = code.replace(**{f"co_{i}": t(j)})
-            elif t == (str,):
-                code = code.replace(**{f"co_{i}": tuple(j.split(":"))})
-    return code
+    code_da
--- pro.asm
+++ pro.asm
@@ -1,10 +1,19 @@
 .code
-  load_global print_fun
-  load_global print_fun
+  load_global print_fun @ name: "print"
+  load_const helo
   call_function 1
-  return_value 0
+  @ stack:
+  @ 0: "Hello, World"
+  @ 1: print
+  @ call_function: calls indexed element with element at 0
+  @ pop(top) -> stack
+  @ => print("Hello World")
+  @    stack << None
+
+
+  return_value 0 @ None
 .data
-  const marshal helo fa0d48656c6c6f2c20576f726c6421 @ str "Hello, World!"
+  const helo marshal fa0d48656c6c6f2c20576f726c6421 @ str "Hello, World!"
   name print_fun print
 .meta
   stacksize 4
