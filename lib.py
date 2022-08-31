a5a3142fdd0c91b96ebdd4c41fa8e7927f20fc33
Fabizocker456 <fabian.schiener@gmx.de>
2022-08-31 11:18:40 +0200

    ...
    HEY KIDS!  ANN LANDERS SAYS:
    	Be sure it's true, when you say "I love you".  It's a sin to
    	tell a lie.  Millions of hearts have been broken, just because
    	these words were spoken.

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from dis import *
+import dis
 import sys
 import types, typing
 import regex as re
@@ -30,9 +30,13 @@ def parse(inp:str, code:types.CodeType = None) -> types.CodeType:
             continue
         if mode in modes:
             modes[mode].append((i, line))
-    print(modes)
     # stage III
-    consts = ([], {}) # consts, name: id mapping
+    consts = ([], {}) # ([const], {name: index_of_const})
+    names = ([], {}) # ^
+    flags = 0
+    cellvars = []
+    freevars = ([], {}) # ^^^^
+    locs = ([], {}, [0, 0, 0]) # (*^^^^^, [argc, posargc, kwargc])
     for i, line in modes["data"]:
         typ = i[0]
         cur_name = i[1]
@@ -40,3 +44,119 @@ def parse(inp:str, code:types.CodeType = None) -> types.CodeType:
             sub = i[2]
             if sub == "marshal":
                 item = marshal.loads(bytes.fromhex(i[3]))
+            elif sub == "pickle":
+                item = pickle.loads(bytes.fromhex(i[3]))
+            if item not in consts[0]:
+                consts[0].append(item)
+            consts[1][cur_name] = consts[0].index(item)
+        elif typ == "name":
+            act_name = i[2]
+            if act_name not in names[0]:
+                names[0].append(act_name)
+            names[1][cur_name] = names[0].index(act_name)
+        elif typ == "flag":
+            cmpfln = dis.COMPILER_FLAG_NAMES
+            actflag = {cmpfln[o]: o for o in cmpfln}[act_name.upper()]
+            if len(i) <= 2 or i[2] not in ["xor", "not"]:
+                flags |= actflag
+            elif i[2] == "xor":
+                flags ^= actflag
+            else:
+                flags &= ~actflag
+        elif typ == "cell":
+            cellvars.append(cur_name)
+        elif typ == "free":
+            act_var = i[2]
+            if act_var not in freevars[0]:
+                freevars[0].append(act_var)
+            freevars[1][cur_name] = freevars[0].index(act_var)
+        elif typ == "local":
+            act_var = i[2]
+            if act_var not in locs[0]:
+                locs[0].append(act_var)
+            locs[1][cur_name] = locs[0].index(act_var)
+            if len(i) <= 4:
+                argt = i[3]
+                if argt == "posonly":
+                    locs[2][0] += 1
+                    locs[2][1] += 1
+                elif argt == "kwonly":
+                    locs[2][2] += 1
+                elif argt == "":
+                    locs[2][0] += 1
+
+    ops = []
+    anchors = {}
+    for addr, (i, line) in enumerate(modes["code"]):
+        if i[0].startswith(":"):
+            anchors[i.pop(0)[1:]] = addr
+    for addr, (i, line) in enumerate(modes["code"]):
+        print(i)
+        op = i[0]
+        if op.isdigit() and 0 <= int(op) <= 255:
+            op = int(op)
+        else:
+            op = dis.opmap[i[0].upper()]
+        if op < dis.HAVE_ARGUMENT:
+            ops.append([op, 0])
+            continue
+        arg = i[1]
+        print(addr)
+        if arg.isdigit() and 0 <= int(arg) <= 255:
+            ops.append([op, int(arg)])
+            continue
+        elif op in dis.hascompare:
+            arg = dis.cmp_op.index(arg)
+        elif op in dis.hasname:
+            arg = names[1][arg]
+        elif op in dis.hasfree:
+            arg = freevars[1][arg]
+        elif op in dis.hasconst:
+            arg = consts[1][arg]
+        elif op in dis.haslocal:
+            arg = locs[1][arg]
+        elif op in dis.hasjabs:
+            arg = anchors[arg]
+        elif op in dis.hasjrel:
+            assert(anchors[arg] > addr)
+            arg = anchors[arg] - addr
+        ops.append([op, int(arg)])
+    print(ops)
+    bytecode = b''
+    for i in ops:
+        bytecode += bytes(i)
+    crp = {
+        "code": bytecode,
+        "argcount": locs[2][0],
+        "posonlyargcount": locs[2][1],
+        "kwonlyargcount": locs[2][2],
+        "nlocals": len(locs[0]),
+        "varnames": tuple(locs[0]),
+        "cellvars": tuple(cellvars),
+        "freevars": tuple(freevars[0]),
+        "names": tuple(names[0]),
+        "consts": tuple(consts[0]),
+        "flags": flags,
+        "stacksize": 0,
+        "firstlineno": 1,
+        "linetable": b"\x02\x00" * (len(bytecode) // 2),
+        "filename": "<asm>",
+        "name": "<asm>"
+    }
+    for i, line in modes["meta"]:
+        allow = {
+                "name": str,
+                "filename": str,
+                "stacksize": int,
+                "firstlineno": int,
+                "linetable": bytes
+        }
+        tp = allow[i[0]]
+        if tp == bytes:
+            val = bytes.fromhex(i[1])
+        else:
+            val = tp(i[1])
+        crp[i[0]] = val
+    for i in crp:
+        code = code.replace(**{f"co_{i}": crp[i]})
+    return code
--- pro.asm
+++ pro.asm
@@ -1,20 +1,39 @@
 .code
-  load_global print_fun @ name: "print"
+  :init load_global print_fun @ name: "print"
   load_const helo
   call_function 1
-  @ stack:
-  @ 0: "Hello, World"
-  @ 1: print
-  @ call_function: calls indexed element with element at 0
-  @ pop(top) -> stack
-  @ => print("Hello World")
-  @    stack << None
+  return_value 0
 
+.void
 
-  return_value 0 @ None
+== explanation ==
+
+---
+stack << x: push onto stack
+<< stack: pop top
+<<= stack: pop at arg
+---
+
+1. stack << global "print"
+stack is now
+0 print
+
+2. stack << const "Hello, World!"
+stack is now
+0 "Hello, World!"
+1 print
+
+3. stack << (<<= stack).__call__(<< stack)
+stack is now
+0 None
+
+4. return (<< stack)
+stack is empty
+stop execution
+
 .data
   const helo marshal fa0d48656c6c6f2c20576f726c6421 @ str "Hello, World!"
   name print_fun print
 .meta
-  stacksize 4
-  firstlineno 1
+  stacksize 4 @ 2 too much
+  firstlineno 69 @ because why not
