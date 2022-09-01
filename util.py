f7416b272f9bc013bf290bfa56717161b5cc500c
Fabizocker456 <fabian.schiener@gmx.de>
2022-09-01 11:07:32 +0200

    ...
    Attorney General Edwin Meese III explained why the Supreme Court's Miranda
    decision (holding that subjects have a right to remain silent and have a
    lawyer present during questioning) is unnecessary: "You don't have many
    suspects who are innocent of a crime.  That's contradictory.  If a person
    is innocent of a crime, then he is not a suspect."
    		-- U.S. News and World Report, 10/14/85

--- util.py
+++ util.py
@@ -1,12 +1,50 @@
 #!/usr/bin/python
-import dis, marshal
+import dis, marshal, pickle, readline
 print("""
 Choose:
 1) Print all bytecode instructions
 2) Marshal any object
-
-        """)
+3) Pickle any object
+4) Compile and disassemble input
+""")
 inp = input("> ")
 if inp == "1":
+    maxlen = max(map(len, dis.opmap.keys()))
     for i in dis.opmap:
-        print(f"{i} [{dis.opmap[i]}]")
+        op = dis.opmap[i]
+        arg = ""
+        if op < dis.HAVE_ARGUMENT:
+            arg = "no argument"
+        elif op in dis.hasjrel:
+            arg = "relative jump"
+        elif op in dis.hasjabs:
+            arg = "absolute jump"
+        elif op in dis.hasfree:
+            arg = "free variable"
+        elif op in dis.hasname:
+            arg = "name"
+        if arg:
+            arg = f" ({arg})"
+        i = i.lower()
+        i = i.ljust(maxlen)
+        sop = f"{op}".rjust(3, "0")
+        print(f"[{sop}] {i}{arg}")
+        
+
+elif inp == "2":
+    c = eval(input(">>> "))
+    print(marshal.dumps(c).hex())
+elif inp == "3":
+    c = eval(input(">>> "))
+    print(pickle.dumps(c).hex())
+elif inp == "4":
+    print("enter \"end\" to stop")
+    ls = []
+    while 1:
+        c = input(">>> ")
+        if c == "end":
+            break
+        ls.append(c)
+    ls = "\n".join(ls)
+    c = compile(ls, "<asm>", "exec")
+    dis.dis(c)
